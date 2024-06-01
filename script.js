0a18943290311dee79d8ceabdfe11355b61d6553
the-abra <aegean.sword1@gmail.com>
2024-06-01 23:21:23 +0300

    p

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 // challenge https://www.frontendmentor.io/challenges/fylo-dark-theme-landing-page-5ca5f2d21e82137ec91a50fd
+function gogithub() {
+    document.getElementsByClassName('layer')[0].style.display = 'flex'; window.location.href = 'https://www.github.com/projecthostingtool'
+}
